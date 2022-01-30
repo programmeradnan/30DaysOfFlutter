98e763a32447f99c15bae033435ba2d17a5de2f9
Adnan <programmer.adnan@gmail.com>
2022-01-30 18:19:30 +0500

    commit -m "Day 2 Completed"

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalogue/home_page.dart';
 
 
 void main(){
@@ -11,13 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-        child: Container(
-          child: Text('Welcome to 30 days of fltter'),
-        ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
