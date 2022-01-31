b0be44516b6ada2ef227f3e1cc8ea069662d484b
Adnan <programmer.adnan@gmail.com>
2022-01-31 23:29:03 +0500

    commit -m "Day 3 Completed"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogue/home_page.dart';
+import 'package:flutter_catalogue/pages/home_page.dart';
+import 'package:flutter_catalogue/pages/login_page.dart';
 
 
 void main(){
@@ -12,7 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      // home: Homepage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context)=> LoginPage(),
+        "/home" : (context)=> Homepage(),
+        "/login" : (context)=> LoginPage(),
+      },
     );
   }
 }
