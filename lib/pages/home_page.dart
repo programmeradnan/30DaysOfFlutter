b874e73c11936a89862bdf8cdcd349d434ee1c5b
programmeradnan <programmer.adnan@gmail.com>
2022-02-07 23:07:28 +0500

    commit -m "Day 13 Completed"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalogue/models/catalogue.dart';
 import 'package:flutter_catalogue/widgets/drawer.dart';
 import 'package:flutter_catalogue/widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final int days=30;
+
   final String name="Codepur";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    final catalogueJson = await rootBundle.loadString("assets/files/catalogue.json");
+    final decodeData = jsonDecode(catalogueJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
